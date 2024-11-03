9594093e727f9c48ee035a07b53b4a8c803be92c
Lathif Nur Hidayat <lathifnurhidayat09@gmail.com>
2024-11-04 01:23:16 +0700

    Add Category UI

--- lib/page/category_page.dart
+++ lib/page/category_page.dart
@@ -1,15 +1,73 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class CategoryPage extends StatefulWidget {
+  const CategoryPage({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<CategoryPage> createState() => _CategoryPageState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _CategoryPageState extends State<CategoryPage> {
+  bool _switchValue = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Switch(
+                value: _switchValue,
+                onChanged: (bool value) {
+                  setState(() {
+                    _switchValue = value;
+                  });
+                },
+                inactiveTrackColor: Colors.grey[500],
+                inactiveThumbColor: const Color.fromARGB(255, 216, 214, 214),
+                activeColor: Colors.orange,
+              ),
+              IconButton(onPressed: () {}, icon: Icon(Icons.add))
+            ],
+          ),
+          Card(
+              elevation: 10,
+              child: ListTile(
+                leading: Icon(
+                  Icons.upload,
+                  color: Colors.red[400],
+                ),
+                title: Text("Sedekah"),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.edit))
+                  ],
+                ),
+              )),
+              Card(
+              elevation: 10,
+              child: ListTile(
+                leading: Icon(
+                  Icons.download,
+                  color: Colors.orange,
+                ),
+                title: Text("Sedekah"),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.edit))
+                  ],
+                ),
+              ))
+        ],
+      ),
+    ));
   }
 }
--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -1,6 +1,8 @@
+import 'package:aplikasi_pencatatan_keuangan/page/category_page.dart';
 import 'package:aplikasi_pencatatan_keuangan/page/home_page.dart';
 import 'package:calendar_appbar/calendar_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -10,34 +12,68 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Widget> _children = [HomePage(), CategoryPage()];
+  int _currentIndex = 0;
+
+  void onTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CalendarAppBar(
-        accent: Colors.orange,
-        backButton: false,
-        locale : "id",
-        onDateChanged: (value) => print(value),
-        firstDate: DateTime.now().subtract(Duration(days : 150)),
-        lastDate:DateTime.now(),
+      appBar: (_currentIndex == 0)
+          ? CalendarAppBar(
+              accent: Colors.orange,
+              backButton: false,
+              locale: "id",
+              onDateChanged: (value) => print(value),
+              firstDate: DateTime.now().subtract(Duration(days: 150)),
+              lastDate: DateTime.now(),
+            )
+          : PreferredSize(
+              preferredSize: Size.fromHeight(100),
+              child: Container(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 45, horizontal: 16),
+                  child: Text(
+                    "Kategori",
+                    style: GoogleFonts.montserrat(fontSize: 25),
+                  ),
+                ),
+              )),
+      body: _children[_currentIndex],
+      floatingActionButton: Visibility(
+        visible: _currentIndex == 0 ? true : false,
+        child: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.orange,
+            shape: CircleBorder(),
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            )),
       ),
-
-      body: HomePage(),
-
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.orange,
-          shape: CircleBorder(),
-          child: Icon(Icons.add, color: Colors.white,)),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
         height: 60,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+            IconButton(
+                onPressed: () {
+                  onTapped(0);
+                },
+                icon: Icon(Icons.home)),
             SizedBox(width: 100),
-            IconButton(onPressed: () {}, icon: Icon(Icons.list))
+            IconButton(
+                onPressed: () {
+                  onTapped(1);
+                },
+                icon: Icon(Icons.list))
           ],
         ),
       ),
